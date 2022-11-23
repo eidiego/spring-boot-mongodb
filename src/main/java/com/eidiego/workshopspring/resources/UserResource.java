115a48b8afa1e3be365c52d86116d13d0c715581
Diego Rodrigues Barbosa <diegokod@gmail.com>
2022-11-23 11:11:53 -0300

    Feat: Method findById with custom exception

--- src/main/java/com/eidiego/workshopspring/resources/UserResource.java
+++ src/main/java/com/eidiego/workshopspring/resources/UserResource.java
@@ -5,9 +5,7 @@ import com.eidiego.workshopspring.dto.UserDTO;
 import com.eidiego.workshopspring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,8 +20,17 @@ public class UserResource {
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).collect(Collectors.toList());
 
         return ResponseEntity.ok().body(listDto);
     };
+
+    @RequestMapping(value="/{id}", method= RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
+    };
+
+
 }
---
+++ src/main/java/com/eidiego/workshopspring/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.eidiego.workshopspring.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
